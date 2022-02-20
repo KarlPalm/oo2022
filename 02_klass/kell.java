afa5e6c2d8a83285bd1d7ec9be17fe3be6a5b768
Karl Voldemar Palm <70939598+KarlPalm@users.noreply.github.com>
2022-02-20 18:19:25 +0200

    Create kell.java

---
+++ 02_klass/kell.java
@@ -0,0 +1,34 @@
+public class kell {
+	private int x, y, tund;
+	String üks = "";
+	String kaks = "";
+	String kolm = "";
+
+	public void kaksteist(){
+			String üks = "  |  ";
+			String kaks = "  o  ";
+			String kolm = "     ";
+	}
+	public void kolm(){
+			String üks = "   ";
+			String kaks = "  o- ";
+			String kolm = "     ";
+	}
+	public void kuus(){
+			String üks = "     ";
+			String kaks = "  o  ";
+			String kolm = "  |  ";
+	}
+	public void üheksa(){
+			String üks = "     ";
+			String kaks = " -o  ";
+			String kolm = "     ";
+	}
+
+
+	public void kysiüks(){return üks;}
+	public void kysikaks(){return kaks;}
+	public void kysikolm(){return kolm;}
+	
+	
+}
