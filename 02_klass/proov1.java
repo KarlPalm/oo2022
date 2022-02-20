01caa32be6c7c22d8b635630cd92db07b606532f
Karl Voldemar Palm <70939598+KarlPalm@users.noreply.github.com>
2022-02-20 18:22:41 +0200

    Add files via upload

---
+++ 02_klass/proov1.java
@@ -0,0 +1,25 @@
+public class proov1{
+	public static void main(String[] arg){
+		int[][] laud=new int[3][3];
+
+
+		int juhis=12;
+
+		kell k1=new kell();
+		
+		  if(juhis == 12){k1.kaksteist();}
+		  else if (juhis == 3){k1.kolm();}
+		  else if (juhis == 6){k1.kuus();}
+		  else if (juhis == 9){k1.üheksa();}
+		  
+		
+		System.out.println("  -12 -   ");
+		System.out.println(" /"k1.kysiüks(); "\\ ");
+		System.out.println(" |9"k1.kysikaks(); "3| ");
+		System.out.println(" \\"k1.kysikolm(); "/  ");
+		System.out.println("   -6-   ");
+		
+		
+	}
+	
+}
